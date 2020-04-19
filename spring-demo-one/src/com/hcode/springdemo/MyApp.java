3ff6cc90b4e61c1863aef41c9e8d5a28a70f5ca5
student8202 <student8202@gmail.com>
2020-04-19 18:36:10 +0700

    applicationcontext

--- spring-demo-one/src/com/hcode/springdemo/MyApp.java
+++ spring-demo-one/src/com/hcode/springdemo/MyApp.java
@@ -5,7 +5,7 @@ public class MyApp {
 	public static void main(String[] args) {
 		
 		// crate the object
-		ICoach theCoach = new BaseballCoach();
+		ICoach theCoach = new TrackCoach();
 		
 		// use the object
 		System.out.println(theCoach.getDailyWorkout());
