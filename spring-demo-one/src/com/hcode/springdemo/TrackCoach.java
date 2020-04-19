bd96eb252039038ccb52bd787f7f20268917975d
student8202 <student8202@gmail.com>
2020-04-19 17:58:26 +0700

    rough prototype 2

---
+++ spring-demo-one/src/com/hcode/springdemo/TrackCoach.java
@@ -0,0 +1,11 @@
+package com.hcode.springdemo;
+
+public class TrackCoach implements ICoach {
+
+	@Override
+	public String getDailyWorkout() {
+		// TODO Auto-generated method stub
+		return "Run a hard 5 km";
+	}
+
+}
