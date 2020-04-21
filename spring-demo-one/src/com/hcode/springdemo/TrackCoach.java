23c0b1aad46c10d0c20171db19134076edb60cb7
student8202 <student8202@gmail.com>
2020-04-21 21:34:08 +0700

    bean lifecycle

--- spring-demo-one/src/com/hcode/springdemo/TrackCoach.java
+++ spring-demo-one/src/com/hcode/springdemo/TrackCoach.java
@@ -24,5 +24,14 @@ public class TrackCoach implements ICoach {
 	public String getDailyFortune() {
 		return "Just Do It: " + fortuneService.getFortune();
 	}
-
+	
+	// add an init method
+	public void doMyStartupStuff() {
+		System.out.println("TrackCoach: inside method doMyStartupStuff");
+	}
+	
+	// add a destroy method
+	public void doMyCleanUpStuffYoYo() {
+		System.out.println("TrackCoach: inside method doMyCleanUpStuffYoYo");
+	}
 }
