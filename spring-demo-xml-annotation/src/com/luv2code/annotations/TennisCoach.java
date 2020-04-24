ba1dcbaa42977857dbdc20f7a344c14e5bf31dfc
student8202 <student8202@gmail.com>
2020-04-24 23:31:30 +0700

    DI setter injection

--- spring-demo-xml-annotation/src/com/luv2code/annotations/TennisCoach.java
+++ spring-demo-xml-annotation/src/com/luv2code/annotations/TennisCoach.java
@@ -8,10 +8,21 @@ public class TennisCoach implements ICoach {
 	
 	private IFortuneService myForturneService;
 	
+	// define a default constructor
+	public TennisCoach() {
+		System.out.println(">> TennisCoach: inside default constructor");
+	}
+	
+	@Autowired
+	public void setMyFortuneService(IFortuneService myFortuneService) {
+		this.myForturneService = myFortuneService;
+	}
+	/*
 	@Autowired
 	public TennisCoach(IFortuneService myForturneService) {
 		this.myForturneService = myForturneService;
 	}
+	*/
 
 	@Override
 	public String getDailyWorkout() {
