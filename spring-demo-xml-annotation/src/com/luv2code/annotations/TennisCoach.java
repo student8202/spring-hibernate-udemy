e9238697851ef4fc71ddda62a2eb77df10ddc130
student8202 <student8202@gmail.com>
2020-04-24 23:39:40 +0700

    DI field injection

--- spring-demo-xml-annotation/src/com/luv2code/annotations/TennisCoach.java
+++ spring-demo-xml-annotation/src/com/luv2code/annotations/TennisCoach.java
@@ -6,25 +6,32 @@ import org.springframework.stereotype.Component;
 @Component()
 public class TennisCoach implements ICoach {
 	
+	@Autowired
 	private IFortuneService myForturneService;
 	
 	// define a default constructor
 	public TennisCoach() {
 		System.out.println(">> TennisCoach: inside default constructor");
 	}
+	
+	/*
 	// define a setter method
 	@Autowired
 	public void setMyFortuneService(IFortuneService myFortuneService) {
 		System.out.println(">> TennisCoach: inside setMyFortuneService method");
 		this.myForturneService = myFortuneService;
 	}
+	*/
 	
+	/*
 	// define a any method
 		@Autowired
 		public void doAnyCrazyStuff(IFortuneService myFortuneService) {
 			System.out.println(">> TennisCoach: inside doAnyCrazyStuff method");
 			this.myForturneService = myFortuneService;
 		}
+		*/
+	
 	/*
 	@Autowired
 	public TennisCoach(IFortuneService myForturneService) {
