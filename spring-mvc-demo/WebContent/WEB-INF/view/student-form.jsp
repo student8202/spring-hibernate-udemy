4707bb268c2cd15e1870c46ccc127f7c51a460af
student8202 <student8202@gmail.com>
2020-04-28 12:38:28 +0700

    form tags dropdown lists use properties file

--- spring-mvc-demo/WebContent/WEB-INF/view/student-form.jsp
+++ spring-mvc-demo/WebContent/WEB-INF/view/student-form.jsp
@@ -12,7 +12,7 @@
 	Last name: <form:input path="lastName" />
 	<br/><br/>
 	<form:select path="country">
-		<form:options items="${student.countryOptions}"/>
+		<form:options items="${theCountryOptions}"/>
 	</form:select>
 	<br/><br/>
 	<input type="submit" value="Submit" />
--- spring-mvc-demo/src/com/luv2code/springdemo/mvc/StudentController.java
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/StudentController.java
@@ -1,5 +1,9 @@
 package com.luv2code.springdemo.mvc;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -9,6 +13,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/student")
 public class StudentController {
 	
+	@Value("#{countryOptions}") 
+	private Map<String, String> countryOptions;
+	
 	@RequestMapping("/showForm")
 	public String showForm(Model theModel) {
 		
@@ -17,6 +24,9 @@ public class StudentController {
 		
 		// add student object to the model
 		theModel.addAttribute("student", theStudent);
+		
+		// add the country options to the model 
+	    theModel.addAttribute("theCountryOptions", countryOptions); 
 		
 		return "student-form";
 	}
